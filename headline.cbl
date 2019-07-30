eb1e19a16fa5d6d76ce150bee1ddb5b231fd489a
maryembadri <52162075+maryembadri@users.noreply.github.com>
2019-07-30 12:02:12 +0200

    Update headline.cbl

--- headline.cbl
+++ headline.cbl
@@ -1,14 +1,14 @@
        CBL  SOURCE XREF
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. '${1:mprb05}'. *> this is the name of the COBOL file 
+       PROGRAM-ID. '${1:programID}'. *> this is the name of the COBOL file 
        ************************************************************
        *
        *  ${2:comments of the program}
        *  
        *  
        *
-       *  Program     ${1:mprb05}
-       *  PSB:        ${1:mprb05}
+       *  Program     ${1:programID}
+       *  PSB:        ${1:programID}
        *  Databases:  none
        *  Access:     none
        *
@@ -16,7 +16,8 @@
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SOURCE-COMPUTER.  ${3:IBM-370}.
-       *SOURCE-COMPUTER.  ${3:IBM-370} with debugging mode.
        OBJECT-COMPUTER.  ${3:IBM-370}.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+
+       END PROGRAM ${1:programID}
