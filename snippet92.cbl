4f4e65832d783ba91b895f6bc2920128ca49ad83
zoppsnippets <53005132+zoppsnippets@users.noreply.github.com>
2019-09-19 15:06:40 +0200

    Added file snippet92.cbl

---
+++ snippet92.cbl
@@ -0,0 +1,43 @@
+*-----------------------------------------------------------------
+
+      * Snippet de lecture Db2 standard                  
+
+      *-----------------------------------------------------------------
+
+       LECTURE.
+
+
+           EXEC SQL
+
+           ${1:SQL query}
+
+           END-EXEC
+
+
+           EVALUATE SQLCODE
+
+               WHEN 0
+
+               ${2:Instructions when OK}
+
+               ${2:Instructions when OK}
+
+               WHEN 100
+
+                MOVE 21                 TO 410-CR
+
+                MOVE 'LIGNE INCONNUE'   TO 410-LIB-CR
+
+               WHEN OTHER
+
+                MOVE 99                 TO 410-CR
+
+                MOVE 'ERR GRAVE SELECT' TO 410-LIB-CR
+
+           END-EVALUATE.
+
+      *-----------------------------------------------------------------
+
+      * [DB2_Select] : Snipet de lecture Db2 standard                  -
+
+      *-----------------------------------------------------------------
